fe25e7ff978fb1883367a3d1c73f8c4f50371495
varmadevraj <gvarmadevraj369@gmail.com>
2024-04-27 17:06:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// let userName= window.prompt("hello everyone");
+// console.log(userName)
+
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const countLabel = document.getElementById("countLabel");
+let count = 0;
+
+decreaseBtn.onclick = function(){
+    count--;
+    countLabel.textContent = count;
+}
+
+resetBtn.onclick = function(){
+    count=0;
+    countLabel.textContent = count;
+}
+
+increaseBtn.onclick = function(){
+    count++;
+    countLabel.textContent = count;
+}
